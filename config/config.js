a88669af845a23f267caa352eba5acc0d0ebc6c7
John J. Watson <outlawstar4761@gmail.com>
2024-10-07 20:38:10 +0100

    typo

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-module.exports{
-  encryptionPhrase:'sample'
+module.exports = {
+  encryptionPhrase:'sample',
   ldapSchema:'dc=ldap,dc=example,dc=com',
   ldapDomain:'example.com',
   googleParentFolders:['1klAFSGHQfvrSk3taxl4Qm3AzVATqtaWT']
